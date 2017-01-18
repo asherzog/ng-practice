ce776907427dd9757ebc7620be0c7fa21fcc3b2b
Andy Herzog <asherzog19@gmail.com>
2017-01-18 08:46:09 -0700

    finished http-calls

--- http-calls/app/public/js/expenses.component.js
+++ http-calls/app/public/js/expenses.component.js
@@ -4,10 +4,60 @@
   angular.module('app')
     .component('expenses', {
       controller: controller,
-      template: ``
-    })
+      templateUrl: '../templates/expenses.html'
+    });
+  controller.$inject = ['$http']
 
-  function controller() {
+
+  function controller($http) {
+    const vm = this;
+
+    vm.$onInit = function() {
+      $http.get('/api/expenses')
+            .then((results) => {
+              vm.expenses = results.data;
+            });
+    };
+
+    vm.addExpense = function() {
+      $http.post('/api/expenses', vm.expense)
+            .then((response) => {
+              vm.expenses.push(response.data);
+
+            });
+    };
+
+    vm.updateExpenseFields = function(expense) {
+      vm.editCategory = expense.category;
+      vm.editAmount = expense.amount;
+      vm.id = expense.id;
+
+      vm.updateExpense = function() {
+        vm.newExpense = {
+          category: vm.editCategory,
+          amount: vm.editAmount
+        };
+        $http.patch(`/api/expenses/${vm.id}`, vm.newExpense)
+              .then((response) => {
+                for (var i = 0; i < vm.expenses.length; i++) {
+                  if (vm.expenses[i].id == response.data.id) {
+                    vm.expenses[i] = response.data;
+                  }
+                }
+                delete vm.editCategory;
+                delete vm.editAmount;
+              });
+      };
+    };
+
+
+
+    vm.deleteExpense = function(expense) {
+      $http.delete(`/api/expenses/${expense.id}`)
+            .then((response) => {
+              vm.expenses.splice(vm.expenses.indexOf(expense),1);
+            });
+    };
   }
 
 }());
